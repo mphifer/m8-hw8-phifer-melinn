868b94b218c8646862868085e856eca9e56dff5a
Melinn Phifer <mphifer@ufl.edu>
2020-10-22 13:32:03 -0400

    on hover one submenu appears at a time

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # m8-hw8-phifer-melinn
+
+https://www.w3schools.com/cssref/css3_pr_transition.asp
--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 // It is always helpful to use comments in your code!
+
+var onHover = document.getElementsByClassName('main-menu-labels');
+var showMenu = document.getElementsByClassName('sub-menu');
+
+
+function openOne() {
+  showMenu[0].classList.toggle('show');
+}
+
+onHover[0].addEventListener('mouseenter', openOne);
+onHover[0].addEventListener('mouseleave', openOne);
+
+
+function openTwo() {
+  showMenu[1].classList.toggle('show');
+}
+
+onHover[1].addEventListener('mouseenter', openTwo);
+onHover[1].addEventListener('mouseleave', openTwo);
+
+
+function openThree() {
+  showMenu[2].classList.toggle('show');
+}
+
+onHover[2].addEventListener('mouseenter', openThree);
+onHover[2].addEventListener('mouseleave', openThree);
